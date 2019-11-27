da9ec6d9f01496bc62692770e08f535bc4ba8f9f
MohanKrishna-RC <mohankrishna.landa@quantiphi.com>
2019-11-27 18:01:38 +0530

    Structurizing the Repo

---
+++ codes/triplet_loss.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+import math
+
+def triplet_loss(y_true, y_pred, alpha = 0.3):
+    anchor, positive, negative = y_pred[0], y_pred[1], y_pred[2]
+
+    pos_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, positive)), axis=-1)
+    neg_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, negative)), axis=-1)
+    basic_loss = tf.add(tf.subtract(pos_dist, neg_dist), alpha)
+    loss = tf.reduce_sum(tf.maximum(basic_loss, 0.0))
+   
+    return loss
